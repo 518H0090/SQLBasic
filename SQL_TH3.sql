a54a2bd3db4f6087ffd9cf714e7bf651e5685919
518H0090 <nhoxhieuro5@gmail.com>
2022-04-26 15:28:09 +0700

    Alter table

--- SQL_TH3.sql
+++ SQL_TH3.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE DB_TH3
+﻿CREATE DATABASE DB_TH3
 ON
 (
 	NAME = DB_TH3,
@@ -22,3 +22,30 @@ USE DB_TH3;
 
 GO
 
+CREATE TABLE Student 
+(
+	Id int,
+	Name nvarchar(50),
+	Age int,
+	DateOfBirth date,
+	Gender bit
+)
+
+--Thay đổi trong bản
+--Alter table add
+ALTER TABLE Student 
+add [Address] nvarchar(50)
+
+ALTER TABLE Student 
+add Province nvarchar(50),District nvarchar(50)
+
+--modify column
+ALTER TABLE Student 
+ALTER COLUMN Address nvarchar(255)
+
+--Drop column
+ALTER TABLE Student
+DROP COLUMN Address 
+
+--Rename Column
+sp_rename 'Student.Province','Province1', 'Column'
