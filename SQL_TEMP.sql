62cf18221841f0b83a8b04d18d721240b8e06cf4
518H0090 <nhoxhieuro5@gmail.com>
2022-04-26 15:14:35 +0700

    Temp table

---
+++ SQL_TEMP.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE #sample_temp
+(
+	ID int,
+	[Name] nvarchar(50)
+)
+
+insert into #sample_temp values(1,N'Trung Hiếu');
+
+select * from #sample_temp;
+
+CREATE TABLE ##global_sample_temp
+(
+	ID int,
+	[Name] nvarchar(50)
+)
---
+++ SQL_TH3.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE DB_TH3
+ON
+(
+	NAME = DB_TH3,
+	FILENAME = 'D:\DATA\DB_TH3.mdf',
+	SIZE = 10mb,
+	MAXSIZE = 50mb,
+	FILEGROWTH = 5mb
+)
+LOG ON
+(
+	NAME = DB_TH3_log,
+	FILENAME = 'D:\DATA\DB_TH3_log.ldf',
+	SIZE = 10mb,
+	MAXSIZE = 50mb,
+	FILEGROWTH = 5mb
+)
+
+GO
+
+USE DB_TH3;
+
+GO
+
